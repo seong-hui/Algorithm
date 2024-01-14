4ccf5307c5c901b3adae23896005683550bd7a11
seonghmo <52481403+seong-hui@users.noreply.github.com>
2024-01-14 23:36:57 +0900

    [level Lv.3] Title: 단어 변환, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/Lv.3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/Lv.3/43163. 단어 변환/단어 변환.js
@@ -4,7 +4,23 @@ function solution(begin, target, words) {
     if (!words.includes(target))
         return 0;
     let visited = new Array(words.length).fill(0);
-    answer = dfs(begin , words, target, 0, visited)
+    dfs(begin , words, target, visited, 0);
+    
+    function dfs(cur, words, target, visited, cnt){
+        if(cur === target){
+            answer = cnt;
+            return;
+        }
+        
+        for(let i=0; i< words.length;i++){
+            if (diff_cnt(cur, words[i]) && !visited[i]){
+                visited[i] = true;
+                dfs(words[i], words, target, visited, cnt+1);
+                visited[i] = false;
+            }
+        }
+        return;
+    }
     return answer;
 }
 
@@ -21,20 +37,3 @@ function diff_cnt(word1, word2){
     return 0;
 }
 
-function dfs(cur, words, target, answer, visited) {
-    if (cur === target)
-        return answer;
-
-    for (let i = 0; i < words.length; i++) {
-        if (diff_cnt(cur, words[i]) && !visited[i]) {
-            visited[i] = true;
-            let result = dfs(words[i], words, target, answer + 1, visited);
-            if (result > 0) {
-                visited[i] = false;
-                return result;
-            }
-            visited[i] = false;
-        }
-    }
-    return 0;
-}
