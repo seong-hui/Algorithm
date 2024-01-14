63e73181ea4281786f3c836f0a7c92cd5ce32732
seonghmo <52481403+seong-hui@users.noreply.github.com>
2024-01-14 22:35:27 +0900

    [level Lv.3] Title: 단어 변환, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/Lv.3/43163. 단어 변환/단어 변환.js
@@ -3,38 +3,38 @@ function solution(begin, target, words) {
     
     if (!words.includes(target))
         return 0;
-    answer = dfs(begin , words, target, 0)
+    let visited = new Array(words.length).fill(0);
+    answer = dfs(begin , words, target, 0, visited)
     return answer;
-    
-    function diff_cnt(word1, word2){
-        let diff = 0;
-        for(let i =0 ; i< word1.length; i++){
-            if(word1[i] !== word2[i])
-                diff++;
-            if(diff > 1)
-                return 0;
-        }
-        if(diff === 1)
-            return 1;
-        return 0;
+}
+
+function diff_cnt(word1, word2){
+    let diff = 0;
+    for(let i =0 ; i< word1.length; i++){
+        if(word1[i] !== word2[i])
+            diff++;
+        if(diff > 1)
+            return 0;
     }
-    
-    function dfs(cur, words, target, answer){
-        let minCnt = 0;
-        if(cur === target)
-            return answer
-        for(let i=0; i< words.length;i++){
-            if(diff_cnt(cur, words[i])){
-                const newWords = words.slice(0);
-                newWords.splice(i, 1);
-                let result = dfs(words[i], newWords, target, answer+1);
-                if (result > 0 && (minCnt === 0 || result < minCnt)) {
-                    minCnt = result;
-                }
+    if(diff === 1)
+        return 1;
+    return 0;
+}
+
+function dfs(cur, words, target, answer, visited) {
+    if (cur === target)
+        return answer;
+
+    for (let i = 0; i < words.length; i++) {
+        if (diff_cnt(cur, words[i]) && !visited[i]) {
+            visited[i] = true;
+            let result = dfs(words[i], words, target, answer + 1, visited);
+            if (result > 0) {
+                visited[i] = false;
+                return result;
             }
+            visited[i] = false;
         }
-        return minCnt
     }
-    
- 
+    return 0;
 }
