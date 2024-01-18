c77bdb5e5954d285620e22ea9f3fcd1f7b519ae3
seonghmo <52481403+seong-hui@users.noreply.github.com>
2024-01-18 09:07:46 +0900

    [level 4] Title: 도둑질, Time: 48.43 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.js
+++ 프로그래머스/4/42897. 도둑질/도둑질.js
@@ -1,25 +1,20 @@
 function solution(money) {
-    var answer = 0;
-    let dp = {};
-    dp[0] = money[0];
-    dp[1] = money[0];
+    let dp1 = {};
+    dp1[0] = money[0];
+    dp1[1] = money[0];
     
     for(let i =2; i<money.length-1; i++){
-        dp[i] = Math.max(dp[i-2] + money[i], dp[i-1]);
-    }
-    let dp1 = {};
-    dp1[0] = 0
-    dp1[1] = money[1];
-    
-    for(let i =2; i<money.length; i++){
         dp1[i] = Math.max(dp1[i-2] + money[i], dp1[i-1]);
     }
-    console.log(dp[money.length - 2], dp1[money.length - 1])
     
-    return Math.max(dp[money.length - 2], dp1[money.length - 1]);
-//     answer = dp[money.length-1];
+    let dp2 = {};
+    dp2[0] = 0
+    dp2[1] = money[1];
     
-//     return answer;
+    for(let i =2; i<money.length; i++){
+        dp2[i] = Math.max(dp2[i-2] + money[i], dp2[i-1]);
+    }
+    
+    return Math.max(dp1[money.length - 2], dp2[money.length - 1]);
 }
 
-// 1 2 3 1 7
