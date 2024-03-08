162cab98283a723d115ca565b7bf8bb500b83762
seonghmo <52481403+seong-hui@users.noreply.github.com>
2024-03-08 12:43:33 +0900

    [level 3] Title: 단어 변환, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -3,18 +3,16 @@ function solution(begin, target, words) {
         return 0;
     
     const queue = [[begin, 0]];
-    // const visited = new Set();
-    const visited = [];
-    
-    
+    const visited = new Set();
+  
     while(queue.length){
         const [currentWord, transCnt] = queue.shift();
         if(currentWord === target)
             return transCnt;
         words.forEach((word)=>{
-            if(!visited.includes(word) && isOneLetterDiff(currentWord, word)){
+            if(!visited.has(word) && isOneLetterDiff(currentWord, word)){
                 queue.push([word, transCnt+1]);
-                visited.push(word);
+                visited.add(word);
             }
         });
     }
@@ -32,3 +30,10 @@ function isOneLetterDiff(word1, word2){
     }
     return diff === 1;
 }
+
+/*
+set : 해시 테이블을 사용해서 값의 존재 확인, 삭제 작업이 O(1)이다.
+array의 경우는 O(n)
+해당 문제는 단어의 개수가 최대 50개여서 유의미한 차이는 없지만
+중복된 값이 없으면서 특정 값의 존재확인, 삭제 작업이 잦은 경우 set을 사용하고자 한다.
+*/
